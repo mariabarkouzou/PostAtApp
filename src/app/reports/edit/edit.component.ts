7e967fe3457db338a99244b57859dd2ac575c1ea
Maria Barkouzou <m.mparkouzou@yahoo.gr>
2021-09-13 15:36:10 +0300

    Edit feature almost ready

--- src/app/reports/edit/edit.component.ts
+++ src/app/reports/edit/edit.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
+  userId: any;
+  userDetails: any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params.subscribe((data) => {
+      this.userId = data.id;
+    });
 
+    if (this.userId !== '') {
+      this.userService
+        .viewUser(this.userId)
+        .toPromise()
+        .then((data) => {
+          this.userDetails = data;
+          Object.assign(this.userDetails, data);
+          console.log(this.userDetails);
+        });
+    }
+  }
 }
