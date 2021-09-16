6ad274177f7bff9ea736d5e25f8364bcb9652045
Maria Barkouzou <m.mparkouzou@yahoo.gr>
2021-09-16 13:42:50 +0300

    Update User function ready

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -5,11 +5,14 @@ import { MatListModule } from '@angular/material/list';
 import { PhotosComponent } from './photos/photos.component';
 import { CommentsComponent } from './comments/comments.component';
 import { CreatePostComponent } from './create-post/create-post.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
+import {
+  MatSnackBarModule,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+} from '@angular/material/snack-bar';
 import { CommentPostComponent } from './comment-post/comment-post.component';
 
 @NgModule({
@@ -18,11 +21,23 @@ import { CommentPostComponent } from './comment-post/comment-post.component';
     PhotosComponent,
     CommentsComponent,
     CreatePostComponent,
-    CommentPostComponent
+    CommentPostComponent,
   ],
-  imports: [CommonModule, MatSnackBarModule,MatListModule,MatGridListModule,MatCardModule,MatFormFieldModule,FormsModule, ReactiveFormsModule],
-  providers: [ { 
-    provide: MAT_SNACK_BAR_DEFAULT_OPTIONS , useValue: { duration: 2500}
-  }]
+  imports: [
+    CommonModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: { duration: 2500 },
+    },
+  ],
 })
 export class PostsModule {}
